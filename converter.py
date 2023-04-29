84d7bfb4d3d51cf7b59a914aaa407962ba2dd2c3
Jessica Dea <jessicardea@gmail.com>
2023-04-29 15:59:19 -0700

    Added comments

--- converter.py
+++ converter.py
@@ -1,10 +1,17 @@
+# python image library
 from PIL import Image
 
 import glob
 
+# print current png files
 print(glob.glob('*.png'))
 
 for file in glob.glob('*.png'):
+    # open image file
     im = Image.open(file)
+
+    # convert to RGB file format
     rgb_im = im.convert('RGB')
+
+    # save jpg file
     rgb_im.save(file.replace('png', 'jpg'), quality=95)
